65e7c84c843c20c6847c6d2d769177a32e24194b
Nitesh Anand <nkanand4@gmail.com>
2015-05-07 02:55:18 -0500

    Collector collects the selected image.

--- res/js/FolderCtrlr.js
+++ res/js/FolderCtrlr.js
@@ -10,28 +10,81 @@ angular.module('DataServices', [
     // set a custom template
     LightboxProvider.templateUrl = '_partials/_lightbox.html';
 })
-.controller('FolderCtrlr', function($scope, $log, connector, Lightbox) {
-    var files = [];
+.controller('FolderCtrlr', function($scope, $log, connector, Lightbox, Collector) {
+
     $scope.folderpath = '/Users/nitesh/tmp/screenshots';
     $scope.filter = 'png';
     $scope.sendinfo = function() {
-        files = [];
-        $scope.files = connector.send('setfolder', {
+        Collector.reset();
+        connector.send('setfolder', {
             path: $scope.folderpath,
             filter: $scope.filter
         });
     };
     $scope.getFiles = function() {
-        return files;
+        return Collector.getFiles();
     };
 
     connector.getConnection().on('found', function(message) {
         $log.log('Getting messages');
-        files.push(message);
+        Collector.addFiles(message);
         $scope.$evalAsync();
     });
 
     $scope.openLightboxModal = function (index) {
         Lightbox.openModal($scope.getFiles(), index);
     };
+
+    $scope.isAdded = function() {
+        return Collector.isAlreadyAdded(Lightbox.image);
+    };
+})
+.factory('Collector', function($log) {
+    var files = [];
+    return {
+        reset: function() {
+            files = [];
+        },
+        addFiles: function(file) {
+            files.push(file);
+        },
+        getFiles: function() {
+            return files;
+        },
+        toggleCollect: function(file) {
+            if(this.isAlreadyAdded(file)) {
+                file.isAdded = false;
+            }else {
+                file.isAdded = true;
+            }
+        },
+        isAlreadyAdded: function(file) {
+            var result = !!file.isAdded;
+            $log.info('File added? ', result);
+            return result;
+        }
+    };
+})
+.controller('OverlayController', function($scope, Lightbox, Collector, $log, $document) {
+
+    $scope.selectMe = function() {
+        Collector.toggleCollect(Lightbox.image);
+    };
+    $scope.isAdded = function() {
+        return Collector.isAlreadyAdded(Lightbox.image);
+    };
+
+    $document.bind('keypress', function (event) {
+        if(event.which === 32) {
+            $scope.selectMe();
+            $scope.$apply();
+            event.preventDefault();
+        }
+    });
+
+    $scope.$on('$destroy', function() {
+        $log.log('I am killed');
+        $document.unbind('keypress');
+    });
+
 });
